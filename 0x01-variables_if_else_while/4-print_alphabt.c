afd9577f27bf2303dd361d32b22bf7a65fb4892e
Your Name <you@example.com>
2023-06-16 16:22:14 -0700

    task

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,10 @@ int main(void)
 	int i;
 
 	for (i = 97; i < 123; i++)
-		putchar(i);
-
-	for (i = 65; i < 91; i++)
-		putchar(i);
-
+	{
+		if (i != 101 && i != 113)
+			putchar(i);
+	}
 	putchar(10);
 
 	return (0);
