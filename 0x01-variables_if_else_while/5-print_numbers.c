d4e725a4e29320cd90ce2bc003af0220c8195da2
Your Name <you@example.com>
2023-06-16 16:36:06 -0700

    task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+
+	putchar(10);
+
+	return (0);
+}
