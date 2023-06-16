815902cbb545a044dd52c62580bc00283adfe6fd
Your Name <you@example.com>
2023-06-16 06:48:38 -0700

    task

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - print whether the number stored in the variable n is positive or negative.
+* main - print whether the number is positive or negative.
 *
 * Return: Always 0 (success)
 */
