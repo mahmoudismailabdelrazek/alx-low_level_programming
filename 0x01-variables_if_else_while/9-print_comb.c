4f053a10491644d5c77b968c335c6caabecbcbe0
Your Name <you@example.com>
2023-06-16 17:45:14 -0700

    task

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,15 +11,15 @@ int main(void)
 
 	for (i = 48; i < 58; i++)
 	{
+		putchar(i);
 		if (i < 57)
 		{
-			putchar(i);
 			putchar(44);
 			putchar(32);
 		}
-		else 
+		else
 		{
-			putchar(i);
+			;
 		}
 	}
 
